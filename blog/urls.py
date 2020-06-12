711335316dffe27267985d9ae328d84d810fb1b1
Wayne Kipngeno <kipngenowayne@gmail.com>
2020-06-12 14:19:27 +0300

    inital commit of my Blog project from Django by example

---
+++ blog/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from .views import post_detail, PostListView
+
+app_name = 'blog'
+
+urlpatterns = [
+    #path('',post_list, name='post_list'),
+    path('', PostListView.as_view(), name='post_list'),
+    path('<int:year>/<int:month>/<int:day>/<slug:post>/',
+            post_detail,
+            name='post_detail'),
+]
